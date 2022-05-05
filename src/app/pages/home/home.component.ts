279bce0dec8cc5f926bac0e58aea8cbdfbbdd51b
Zwei1012 <duonghuulai1012@gmail.com>
2022-05-05 15:31:17 +0700

    add pagination

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../../models/movie';
+import { Tv } from '../../models/tv';
 import { MoviesService } from '../../services/movies.service';
 
 @Component({
@@ -11,18 +12,22 @@ export class HomeComponent implements OnInit {
   popularMovies: Movie[] = [];
   upcomingMovies: Movie[] = [];
   topRatedMovies: Movie[] = [];
+  popularTvShows: Tv[] = [];
 
   constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
-    this.moviesService.getMovies('popular').subscribe((response: any) => {
-      this.popularMovies = response.results;      
+    this.moviesService.getMovies('popular').subscribe((movies) => {
+      this.popularMovies = movies;
     });
-    this.moviesService.getMovies('top_rated').subscribe((response: any) => {
-      this.topRatedMovies = response.results;      
+    this.moviesService.getMovies('top_rated').subscribe((movies) => {
+      this.topRatedMovies = movies;
     });
-    this.moviesService.getMovies('upcoming').subscribe((response: any) => {
-      this.upcomingMovies = response.results;      
+    this.moviesService.getMovies('upcoming').subscribe((movies) => {
+      this.upcomingMovies = movies;
+    });
+    this.moviesService.getTvs('popular').subscribe((tvShows) => {
+      this.popularTvShows = tvShows;
     });
   }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,14 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MovieDto } from '../models/movie';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { TvDto } from '../models/tv';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
   baseUrl: string = 'https://api.themoviedb.org/3';
-  apiKey: string = '077e980409a83251fbc8f27d87f41974';
+  apiKey: string = '8c247ea0b4b56ed2ff7d41c9a833aa77';
+
   constructor(private http: HttpClient) {}
 
-  getMovies(type: string = 'upcoming') {
-    return this.http.get(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`);
+  getMovies(type: string = 'upcoming', count: number = 12) {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results.slice(0, count));
+      })
+    );
+  }
+
+  searchMovies(page: number) {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/popular?page=${page}&api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results);
+      })
+    );
+  }
+
+  getTvs(type: string = 'latest', count: number = 12) {
+    return this.http.get<TvDto>(`${this.baseUrl}/tv/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results.slice(0, count));
+      })
+    );
   }
 }
